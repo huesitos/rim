f44e9e6c44fb54b454001aad18086de5e85336bf
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2015-06-02 21:54:26 -0400

    Added index, create, show and edit actions
    Which meand that now is possible to create, list, show and edit projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,47 @@
 class ProjectsController < ApplicationController
+
+  def index
+    @project = Project.all.entries
+  end
+
   def new
     @project = Project.new
+  end
+
+  def edit
+    @project = Project.find params[:id]
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :scope)
   end
 end

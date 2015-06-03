03e8bd0e470cf88ab4c7a9ad33e419a03009b795
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2015-06-02 21:43:24 -0400

    Added project model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :projects
+  #get 'projects/index'
+
+  #get 'projects/new'
+
+  #get 'projects/show'
+
+  #get 'projects/delete'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

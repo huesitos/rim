26f3375e8b940053bba58b3847b02650d1ac9c31
Denisse Lara <dmlaramartin@gmail.com>
2015-06-05 01:45:24 -0400

    Make projects index root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :projects do
     resources :use_cases
+    resources :test_cases
   end
   #get 'projects/index'
 
@@ -14,7 +15,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'projects#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

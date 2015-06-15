9901a366a808f1b46966052ca54e0cc85cdbc9bf
Denisse Lara <dmlaramartin@gmail.com>
2015-06-15 14:44:14 -0400

    Delete comments and add date to comment to identify

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,9 +8,16 @@ class CommentsController < ApplicationController
   def create
     @project = Project.find params[:project_id]
     @issue = @project.issues.find params[:issue_id]
-    @comment = @issue.comments.create(comment_params)
+    @comment = @issue.comments.create(body: comment_params[:body], date: Date.today)
 
     redirect_to project_issue_path @project, @issue
+  end
+
+  def destroy
+    issue = Issue.find(params[:issue_id])
+    issue.comments.find(params[:id]).destroy
+
+    redirect_to project_issue_path(params[:project_id], params[:issue_id]), notice: 'Comment was successfully destroyed.', alert: 'success'
   end
 
   def edit
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment
   include Mongoid::Document
   field :body, type: String
+  field :date, type: Date
+  
   embedded_in :issue
 end
